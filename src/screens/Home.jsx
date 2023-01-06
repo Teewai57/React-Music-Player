b9c1c82ba7b43e1747a66a5f62d3fa73dbd6ac28
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2023-01-06 23:20:46 +0100

    cchore: created vital screens and imported them into home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Favorites from './Favorites'
+import Feed from './Feed'
+import Library from './Library'
+import Player from './Player'
+import Trending from './Trending'
 
 function Home() {
   return (
     <Router>
         <Routes>
             <Route path="/" element={<Library />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/trending" element={<Trending />} />
+            <Route path="/player" element={<Player />} />
+            <Route path="/favorites" element={<Favorites />} />
         </Routes>
     </Router>
   )
