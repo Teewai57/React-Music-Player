55daeb2480616f609046f8a98434c354899da96c
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2023-03-06 22:33:46 +0100

    chore:added props to sidebar buttons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./Sidebar.css"
 import SidebarButton from './SidebarButton'
+import {FaGripfire, FaPlay} from "react-icons/fa"
+import {MdFavorite} from "react-icons/md";
+import { FaSignOutAlt } from 'react-icons/fa';
+import {IoLibrary} from "react-icons/io5";
+import { MdSpaceDashboard } from 'react-icons/md';
 
 function Sidebar() {
   return (
@@ -11,13 +16,13 @@ function Sidebar() {
         className='profile-img'
          />
          <div>
-          <SidebarButton title="" to="" icon=""/>
-          <SidebarButton />
-          <SidebarButton />
-          <SidebarButton />
-          <SidebarButton />
+          <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />}/>
+          <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />}/>
+          <SidebarButton title="Player" to="/player" icon={<FaPlay />}/>
+          <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />}/>
+          <SidebarButton title="Library" to="/" icon={<IoLibrary />}/>
          </div>
-         <SidebarButton />
+         <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />}/>
 
     </div>
   )
