da7a6d8d89da47760d2fcfda070547e16b86f7eb
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2023-05-19 15:27:33 +0100

    initiated spotify api

--- spotify.js
+++ spotify.js
@@ -1,3 +1,13 @@
-const authEndpoint =
-const clientID =
+const authEndpoint = "https://accounts.spotify.com/authorize?"
+const clientId = "6f3b1f88d810477b857651bf41eed2ff"
 const redirectUrl = "http://localhost:3000/"
+
+
+//FOR READING THE LIBRARY AND THE PLAYLISTS
+const scopes = ["user-library-read", "playlist-read-private"]
+
+
+//FOR LOGIN ENDPOINT
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
