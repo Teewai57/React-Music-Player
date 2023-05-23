2bd4f8ff0ccf22477d8a0d537224d892102b6c02
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2023-05-23 15:19:44 +0100

    imported axios and set up client token in spotify.js

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Favorites from '../Favorites/Favorites'
 import Feed from '../Feed/Feed'
@@ -8,22 +9,45 @@ import Trending from '../Trending/Trending'
 import "./Home.css";
 import Sidebar from '../../components/sidebar/Sidebar'
 import Login from '../auth/login'
+import { setClientToken } from "../../spotify";
+
 
 function Home() {
-  return (
-    <Router>
-        <div className="main-body">
-          <Login />
-            {/* <Sidebar />
-        <Routes>
-            <Route path="/" element={<Library />} />
-            <Route path="/feed" element={<Feed />} />
-            <Route path="/trending" element={<Trending />} />
-            <Route path="/player" element={<Player />} />
-            <Route path="/favorites" element={<Favorites />} />
-        </Routes> */}
-        </div>
-    </Router>
+
+  const [token, setToken] = useState("");
+
+  
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
+  }, []);
+
+  return !token ? (
+    <Login />
+  ) : ( 
+     <Router>
+    <div className="main-body">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Library />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </div>
+  </Router>
+
   )
 }
 
--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const authEndpoint = "https://accounts.spotify.com/authorize?"
 const clientId = "6f3b1f88d810477b857651bf41eed2ff"
 const redirectUri = "http://localhost:3000/"
@@ -11,3 +13,15 @@ const scopes = ["user-library-read", "playlist-read-private"]
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
     "%20"
   )}&response_type=token&show_dialog=true`;
+
+
+const apiClient = axios.create({
+  baseURL: "https://api.spotify.com/v1/",
+});
+
+export const setClientToken = (token) => {
+  apiClient.interceptors.request.use(async function (config) {
+    config.headers.Authorization = "Bearer " + token;
+    return config;
+  });
+};
